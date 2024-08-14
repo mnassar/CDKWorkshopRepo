257b9c9c335d733e410fe873842313ad77a6c946
Mohamad Nassar <meb.nassar@gmail.com>
2024-08-14 12:51:39 -0400

    SOME_COMMIT_MESSAGE_HERE

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import aws_cdk as cdk
 
 from cdk_workshop.cdk_workshop_stack import CdkWorkshopStack
 
+from cdk_workshop.pipeline_stack import WorkshopPipelineStack
+
 
 app = cdk.App()
-CdkWorkshopStack(app, "CdkWorkshopStack")
+# CdkWorkshopStack(app, "CdkWorkshopStack")
+WorkshopPipelineStack(app, "WorkshopPipelineStack")
 
 app.synth()
--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,26 +1,37 @@
 from constructs import Construct
+
 from aws_cdk import (
-    Duration,
-    Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+	Stack, 
+	aws_lambda as _lambda, 
+	aws_apigateway as apigw,
 )
 
+from cdk_workshop.hitcounter import HitCounter
+
+from cdk_dynamo_table_view import TableViewer 
 
 class CdkWorkshopStack(Stack):
-
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
-
-        queue = sqs.Queue(
-            self, "CdkWorkshopQueue",
-            visibility_timeout=Duration.seconds(300),
+	def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+		super().__init__(scope, construct_id, **kwargs)
+		# define an AWS lambda resource
+		my_lambda = _lambda.Function(
+			self, "hello_handler", 
+			runtime = _lambda.Runtime.PYTHON_3_8, 
+			code = _lambda.Code.from_asset('lambda'), 
+			handler = 'hello.handler',
+		)
+		
+		hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=my_lambda,
         )
 
-        topic = sns.Topic(
-            self, "CdkWorkshopTopic"
+		apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=hello_with_counter.handler,
         )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+		table_viewer = TableViewer(self, "viewHitCounter",
+            title = "Hello hits", 
+            table = hello_with_counter.table, 
+			sort_by = "-hits", 
+	) 
